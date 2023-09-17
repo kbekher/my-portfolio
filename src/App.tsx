7fbbf2ec0f80c9d0a3cc922c65848341880c5ec6
Kristina Bekher <bekherkr@gmail.com>
2023-09-17 13:39:40 +0200

    Initial commit
    Set up a project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import { Header } from './components/Header';
+// import { About } from './components/About';
+// import { Skills } from './components/Skills/Skills';
+// import { Certificates } from './components/Certificates';
+// import { Projects } from './components/Projects';
+// import { Contacts } from './components/Contacts';
+// import { Footer } from './components/Footer';
 
-export default App;
+import './App.scss';
+
+export const App = () => (
+  <div className="App">
+    {/* <Header /> */}
+
+    <main className="App__main">
+      {/* <About />
+      <Skills />
+      <Certificates />
+      <Projects />
+      <Contacts /> */}
+    </main>
+
+    {/* <Footer /> */}
+  </div>
+);
